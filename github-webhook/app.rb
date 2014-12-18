c801b7f67449c5343d74155badb53df2cf57f372
Jonathan Lozinski <jonathan.lozinski@gmail.com>
2014-12-18 00:15:28 -0500

    Parses JSON from body

--- github-webhook/app.rb
+++ github-webhook/app.rb
@@ -1,12 +1,16 @@
 require 'rack'
+require 'json'
 
 class App
 	def call(env)
 		logger = env["rack.logger"]
 		logger.info "Called!"
 		req = Rack::Request.new(env)
-		logger.info req.body
+		payload = JSON.parse(req.body.read)
+		logger.info payload
 		return [200, {}, []]
+	rescue => e
+		return [500, {}, [e.message]]
 	end
 
 		
