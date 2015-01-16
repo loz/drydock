ebb0500e49f36ffb58dbb34615790ff8cbdd5c6c
Jonathan Lozinski <jonathan.lozinski@gmail.com>
2015-01-16 11:57:21 +0000

    Finished specification of build-spec, should work as general state machine

--- pipeline-manager/lib/pipeline-manager/build-spec.rb
+++ pipeline-manager/lib/pipeline-manager/build-spec.rb
@@ -1,6 +1,37 @@
 require 'yaml'
 						
 module PipelineManager
+	class Step
+		attr_reader :name
+
+		def initialize(details)
+			@name, @spec = details
+		end
+
+		def ready?(state)
+			if state
+				!already_launched?(state) &&
+				dependencies_satisfied?(state)
+			else
+				dependencies.empty?
+			end
+		end
+
+		private
+
+		def dependencies
+			@spec["dependencies"] || []
+		end
+
+		def dependencies_satisfied?(state)
+			dependencies.all? { |d| state[d] == "success" }
+		end
+
+		def already_launched?(state)
+			state[name]
+		end
+	end
+
 	class BuildSpec
 
 		def self.from_yaml(yml)
@@ -13,6 +44,31 @@ module PipelineManager
 
 		def name
 			@structure["name"]
+		end
+
+		def next_steps(state)
+			steps.select { |s| s.ready?(state) }
+		end
+
+		def completed?(state)
+			todo = next_steps(state)
+			todo.empty? && all_finished?(state)
+		end
+
+		def succeeded?(state)
+			state.values.all? { |s| s == "complete" }
+		end
+
+		private
+
+		def all_finished?(state)
+			!state.values.any? {|s| s == "running" }
+		end
+
+		def steps
+			@steps ||= @structure["steps"].map do |s|
+				Step.new(s)
+			end
 		end
 
 	end
--- pipeline-manager/spec/lib/pipeline-manager/build-spec_spec.rb
+++ pipeline-manager/spec/lib/pipeline-manager/build-spec_spec.rb
@@ -27,5 +27,84 @@ steps:
 		it "creates a specification from the string" do
 			expect(subject.name).to eq "Example"
 		end
+
+		describe "#next_steps" do
+			context "with no prior state" do
+				let(:state) { nil }
+
+				it "includes steps with no dependencies" do
+					steps = subject.next_steps(state)
+
+					expect(steps.length).to eq 1
+					step = steps.first
+
+					expect(step.name).to eq 'first-one'
+				end
+			end
+
+			context "with prior state" do
+				let(:state) { {"first-one" => "success"}}
+
+				it "includes steps with satisfied dependencies" do
+					steps = subject.next_steps(state)
+
+					expect(steps.length).to eq 1
+					step = steps.first
+
+					expect(step.name).to eq 'second-one'
+				end
+
+				it "does not include steps not satisfied" do
+					state = { "first-one" => "running" }
+					expect(subject.next_steps(state).length).to eq 0
+				end
+			end
+		end
+
+		describe "#completed?" do
+			context "when there are more steps" do
+				let(:state) { nil }
+
+				it "is not completed" do
+					expect(subject.completed?(state)).to be false
+				end
+			end
+
+			context "when there are no more steps" do
+				context "and some are running" do
+					let(:state) { { "first-one" => "running" } }
+
+					it "is not complete" do
+						expect(subject.completed?(state)).to be false
+					end
+				end
+
+				context "and all have run" do
+					let(:state) { { "first-one" => "complete", "second-one" => "complete" } }
+
+					it "is complete" do
+						expect(subject.completed?(state)).to be true
+					end
+				end
+			end
+		end
+
+		describe "#succeeded?" do
+			context "when all tasks are completed" do
+				let(:state) { { "first-one" => "complete", "second-one" => "complete" } }
+
+				it "is successful" do
+					expect(subject.succeeded?(state)).to be true
+				end
+			end
+
+			context "when one or more tasks is failed" do
+				let(:state) { { "first-one" => "complete", "second-one" => "failed" } }
+
+				it "is not successful" do
+					expect(subject.succeeded?(state)).to be false
+				end
+			end
+		end
 	end
 end
